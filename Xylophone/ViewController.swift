725202881cb4d18fd608e541f3da6c6bee19492d
Suharik001 <suharev-a@list.ru>
2024-07-16 15:22:38 +0300

    Fix

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        func touchDown(){
+            UIView.animate(withDuration: 0.2, animations: {
+                sender.alpha = 0.5
+                print("Start")
+            })
+        }
+        touchDown()
+        func touchUp(){
+            UIView.animate(withDuration: 0.2, animations: {
+                sender.alpha = 1
+                print("End")
+            })
+        }
+        let nameButton = sender.title(for: .normal)!
+        
+        playSound(soundName: nameButton)
+        touchUp()
+    }
+  
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
